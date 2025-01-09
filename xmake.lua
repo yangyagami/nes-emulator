2ec2a9575c7d636ed18ee39ad1ab7cb2045175e5
yangyagami <a651685099@163.com>
2025-01-09 15:07:50 +0800

    Fix bugs.

--- xmake.lua
+++ xmake.lua
@@ -14,7 +14,7 @@ toolchain_end()
 
 add_rules("mode.check", "mode.debug", "mode.release")
 
-add_cxflags("-Wall", "-Werror", "-Wextra", "-std=c++20")
+add_cxflags("-Wall", "-Wextra", "-std=c++20")
 
 add_requires("raylib", "gtest")
 
