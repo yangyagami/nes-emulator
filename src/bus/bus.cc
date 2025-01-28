38ff7ba1d88ea77f638e6506a50c75a5a2ca15fa
yangyagami <a651685099@163.com>
2025-01-28 17:45:33 +0800

    Modify bus.

--- src/bus/bus.cc
+++ src/bus/bus.cc
@@ -8,21 +8,37 @@ namespace nes {
 Bus::Bus(std::array<uint8_t, 0x0800> &memory) : memory_(memory) {}
 
 void Bus::CpuWrite8Bit(uint16_t address, uint8_t value) {
-  memory_[address] = value;
+  if (address >= 0 && address <= 0x0FFF) {
+    memory_[address - 0x0800] = value;
+  } else if (address >= 0x1000 && address <= 0x17FF) {
+    memory_[address - 0x1000] = value;
+  } else if (address >= 0x1800 && address <= 0x1FFF) {
+    memory_[address - 0x1800] = value;
+  } else {
+    memory_[address] = value;
+  }
 }
 
 void Bus::CpuWrite16Bit(uint16_t address, uint16_t value) {
-  memory_[address] = value;
-  memory_[address + 1] = value >> 8;
+  CpuWrite8Bit(address, value);
+  CpuWrite8Bit(address + 1, value >> 8);
 }
 
 uint8_t Bus::CpuRead8Bit(uint16_t address) {
-  return memory_[address];
+  if (address >= 0 && address <= 0x0FFF) {
+    return memory_[address - 0x0800];
+  } else if (address >= 0x1000 && address <= 0x17FF) {
+    return memory_[address - 0x1000];
+  } else if (address >= 0x1800 && address <= 0x1FFF) {
+    return memory_[address - 0x1800];
+  } else {
+    return memory_[address];
+  }
 }
 
 uint16_t Bus::CpuRead16Bit(uint16_t address) {
-  uint16_t ret = memory_[address];
-  ret |= (memory_[address + 1] << 8);
+  uint16_t ret = CpuRead8Bit(address);
+  ret |= (CpuRead8Bit(address + 1) << 8);
   return ret;
 }
 
